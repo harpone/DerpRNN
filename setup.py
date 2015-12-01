e85e33b328d0bbb3668b2c7b0af1b96e78c27ac9
Heikki Arponen <heikki.a.arponen@gmail.com>
2015-12-01 12:36:07 +0200

    USE_CYTHON to False

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ __author__ = 'Heikki Arponen'
 
 # Set USE_CYTHON to True to build extensions using Cython.
 # Set it to False to use the C file
-USE_CYTHON = True
+USE_CYTHON = False
 
 import sys
 
